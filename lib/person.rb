c9e8591a77d009d9152578cf742cf1f455307785
oopanpan <oopanpan@gmail.com>
2021-01-13 16:18:48 -0800

    commit tree d0d889e2423a205c952f05de07321d18da379dc2

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
 # your code goes here
+class Person
+    def initialize(name)
+        @name = name  
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        if @hygiene + 4 <= 10
+            @hygiene += 4
+        else
+            @hygiene = 10
+        end
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+end
